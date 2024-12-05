68a352910b72655b436ecbe12af117848392ee24
Tushar Negi <negitushar81@gmail.com>
2024-12-05 08:34:44 +0530

    react-router

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import './index.css'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Products from './pages/Products';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+
+    {/* navbar */}
+      <div className='text-white flex items-center justify-center p-5 gap-16 font-semibold text-xl'>
+        <Link to={"/home"}>
+          <h2 className='hover:text-gray-400 active:scale-90 cursor-pointer'>Home</h2>
+        </Link>
+        <Link to="/about">
+          <h2 className='hover:text-gray-400 active:scale-90 cursor-pointer'>About</h2>
+        </Link>
+        <Link to="/contact">
+          <h2 className='hover:text-gray-400 active:scale-90 cursor-pointer'>Contact</h2>
+        </Link>
+        <Link to="/products">
+          <h2 className='hover:text-gray-400 active:scale-90 cursor-pointer'>Products</h2>
+        </Link>
+      </div>
+
+
+      {/* routes */}
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ react-router/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../index.css'
+
+const Home = () => {
+  return (
+    <h1>Home</h1>
+  )
+}
+
+export default Home
